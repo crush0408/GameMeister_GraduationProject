0b9356172d4867f5f7001c38fa1ea8c8b8ca9c12
CRUSH04 <msng0408@naver.com>
2022-03-26 17:58:24 +0900

    PlayerHealth

--- Assets/02.Scripts/Player/PlayerHealth.cs
+++ Assets/02.Scripts/Player/PlayerHealth.cs
@@ -2,31 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : LivingEntity
+public class PlayerHealth : LivingEntity
 {
     public float damagedEffectTime = 0.1f;
-    private SpriteRenderer sr;
     private Color temp;
-    DemonAni demonAni;
+    private SpriteRenderer sr;
+
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         temp = sr.color;
-        demonAni = GetComponent<DemonAni>();
-    }
-    private void Update()
-    {
-        /*
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Test1");
-            StartCoroutine(ShowDamagedEffect());
-        }
-        */
     }
     private IEnumerator ShowDamagedEffect()
     {
-        
+
         sr.color = Color.red; // 피격 예시
         Debug.Log(sr.color);
         yield return new WaitForSeconds(damagedEffectTime);
@@ -39,14 +28,12 @@ public class EnemyHealth : LivingEntity
     public override void Die()
     {
         base.Die();
-        demonAni.setDead();
-        //오브젝트 없애는 게 필요할듯 setactive나 destroy
+        // 게임 매니저에서 OnDeath() 액션에 추가
     }
     public override void OnDamage(float damage, Vector2 hitPosition)
     {
         if (isDead) return;
         base.OnDamage(damage, hitPosition);
         StartCoroutine(ShowDamagedEffect());
-        demonAni.setHit();
     }
 }
