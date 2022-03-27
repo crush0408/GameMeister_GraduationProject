9b552be47870873e417f12747a1563f3d025bc25
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-03-27 10:20:08 +0900

    무기 콜라이더 조정, 플레이어 피격시 카메라 흔들림

--- Assets/02.Scripts/Knife/DemonKnife.cs
+++ Assets/02.Scripts/Knife/DemonKnife.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Cinemachine;
+using Cinemachine;
 public class DemonKnife : MonoBehaviour
 {
     //여기서 플레이어 피격 처리?
     //public LayerMask whatIsEnemy;
 
     public int damage;
-    
+    private CinemachineVirtualCamera vCam;
+
+    private void Awake()
+    {
+        vCam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -20,7 +26,14 @@ public class DemonKnife : MonoBehaviour
                 Vector3 contact = collision.transform.position;
 
                 DamageObj.OnDamage(damage, contact);
+                vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 4;
+                Invoke("Return", 0.3f);
             }
         }
+    }
+
+    void Return()
+    {
+        vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
     }
 }
