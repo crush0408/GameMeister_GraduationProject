31fe6c086b3a7b57d1face73ba08b6db7c0f727f
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-03-27 09:33:23 +0900

    시네머신 임포트 왜 오류 뜨냐고오오오오

--- Assets/02.Scripts/Knife/DemonKnife.cs
+++ Assets/02.Scripts/Knife/DemonKnife.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//using Cinemachine;
 public class DemonKnife : MonoBehaviour
 {
     //여기서 플레이어 피격 처리?
     //public LayerMask whatIsEnemy;
 
     public int damage;
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
