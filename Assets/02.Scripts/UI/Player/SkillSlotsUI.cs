cc82b1202ad5cc36d7bb1a0859be7731ec474334
July0313 <choiheejin5959@gmail.com>
2022-03-30 21:55:04 +0900

    SkillSlot

--- Assets/02.Scripts/UI/Player/SkillSlotsUI.cs
+++ Assets/02.Scripts/UI/Player/SkillSlotsUI.cs
@@ -6,19 +6,37 @@ using UnityEngine.UI;
 public class SkillSlotsUI : MonoBehaviour
 {
     public List<Image> skillSlotImage;
+    public List<Text> remainCoolTime;
+
+    private float lerpSpeed;
 
     private PlayerAttack playerAttack;
 
     private void Start()
     {
         playerAttack = GetComponentInParent<PlayerAttack>();
+
+        // UI È®ÀÎ¿ë
+        for (int i = 0; i < skillSlotImage.Count; i++)
+        {
+            playerAttack.skillList[i].remainCoolTime = playerAttack.skillList[i].initCoolTime;
+        }
     }
 
     private void Update()
     {
+        lerpSpeed = 0.2f * Time.time;
+
         for (int i = 0; i < skillSlotImage.Count; i++)
         {
-            skillSlotImage[i].fillAmount = playerAttack.skillList[i].remainCoolTime / playerAttack.skillList[i].initCoolTime;
+            // skillSlotImage[i].fillAmount = playerAttack.skillList[i].remainCoolTime / playerAttack.skillList[i].initCoolTime;
+            /*
+            skillSlotImage[i].fillAmount = Mathf.Lerp(skillSlotImage[i].fillAmount,
+                playerAttack.skillList[i].remainCoolTime / playerAttack.skillList[i].initCoolTime,
+                lerpSpeed);
+            */
+
+            remainCoolTime[i].text = playerAttack.skillList[i].remainCoolTime.ToString();
         }
     }
 }
