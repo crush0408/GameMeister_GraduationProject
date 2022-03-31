c1e815e12b362faf1c187bba4a75247c239b4f1a
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-03-31 12:45:05 +0900

    데몬 왼쪽 피격 안되던거 픽스

--- Assets/02.Scripts/Knife/DemonKnife.cs
+++ Assets/02.Scripts/Knife/DemonKnife.cs
@@ -8,9 +8,34 @@ public class DemonKnife : MonoBehaviour
     //public LayerMask whatIsEnemy;
 
     public int damage;
+    public bool facingRight = false;
+    private Vector2 destination;
+    private EnemyBrain _enemyBrain = null;
 
     private void Awake()
     {
+        _enemyBrain = GetComponentInParent<EnemyBrain>();
+    }
+
+    private void Update()
+    {
+        destination = _enemyBrain.target.position;
+        Vector2 moveDir = destination - (Vector2)transform.position;
+        moveDir = moveDir.normalized;
+        if (facingRight)
+        {
+            if (moveDir.x > 0 && transform.localScale.x < 0 || moveDir.x < 0 && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            if (moveDir.x < 0 && transform.localScale.x < 0 || moveDir.x > 0 && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,5 +54,10 @@ public class DemonKnife : MonoBehaviour
         }
     }
 
-    
+    public void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 }
