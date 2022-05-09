29c695d8b7b6707344c9f4ae62fefc2f048f8b50
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-05-09 18:21:46 +0900

    그라운드 몽키 피벗과 점프 구현 근데 애니는 못함

--- Assets/02.Scripts/Enemy/GroundBossScript.cs
+++ Assets/02.Scripts/Enemy/GroundBossScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GroundBossScript : BossBase
 {
@@ -19,6 +20,7 @@ public class GroundBossScript : BossBase
         attackDistance = 2.5f;
         rightDirection = Vector3.one;
         leftDirection = new Vector3(-rightDirection.x, rightDirection.y, rightDirection.z);
+        jumpPower = 7f;
     }
 
     private void Update()
@@ -48,7 +50,7 @@ public class GroundBossScript : BossBase
             case Global.EnemyFsm.AttackAfter:
                 break;
             case Global.EnemyFsm.PatternMove:
-                base.Jump();
+                Jump();
                 Debug.Log("Jump");
                 ChangeState(Global.EnemyFsm.Delay);
                 break;
@@ -70,5 +72,25 @@ public class GroundBossScript : BossBase
         // 현재 Jump라는 트리거를 만들어둔 상태 (Animator에서는 연결 x)
         // Animation이 JumpUp과 JumpDown있는데, 올라갈때는 JumpUp이 실행되고 내려올때는 JumpDown이 실행되게
         // 그리고 지금 Ground보스 JumpUp 세팅이 이상함
+
+        //myRigid.velocity = new Vector2(myRigid.velocity.x, jumpForce); 걍 위로 점프
+
+        //애니메이션이 안돼요
+        //myAnim.SetFloat("tree", myRigid.velocity.y);
+        //Debug.Log(myRigid.velocity.y);
+        
+        
+        Vector3 startPos = transform.position;
+        Vector3 lastPos = myTarget.transform.position;
+        Vector3 middlePos;
+        if (transform.position.x < myTarget.transform.position.x)
+        {
+            middlePos = startPos + new Vector3(Vector3.Distance(startPos, lastPos) / 2, jumpPower);
+        }
+        else
+        {
+            middlePos = startPos + new Vector3(-(Vector3.Distance(startPos, lastPos)) / 2, jumpPower);
+        }
+        transform.DOPath(new[] { middlePos, startPos + Vector3.up, middlePos + Vector3.left * 0.8f, lastPos + Vector3.up, middlePos + Vector3.right * 1, lastPos + Vector3.up }, 1f, PathType.CubicBezier).SetEase(Ease.Unset);
     }
 }
