59731feca1f7b19bab5643362dbd7895395ebddf
July0313 <choiheejin5959@gmail.com>
2022-05-11 08:45:58 +0900

    Update EnemySkillCollider.cs
    GroundMonk 애니메이션에 콜라이더를 붙였지만 작동은 안되는 상태

--- Assets/02.Scripts/Enemy/EnemySkillCollider.cs
+++ Assets/02.Scripts/Enemy/EnemySkillCollider.cs
@@ -7,22 +7,8 @@ public class EnemySkillCollider : MonoBehaviour
     public float damage = 0f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*
         BoxCollider2D boxCollider = collision.GetComponent<BoxCollider2D>();
         if (collision.gameObject.CompareTag("Player") && collision == boxCollider)
-        {
-            IDamageable target = collision.GetComponent<IDamageable>();
-            if(target != null)
-            {
-                target.OnDamage(damage, transform.position, true);
-                Debug.Log(this.gameObject.name + "이 " + damage + "만큼 피해를 입힘");
-            }
-        }
-        */
-
-        // 적용이 안 되어서 주석 처리함
-
-        if (collision.gameObject.CompareTag("Player"))
         {
             IDamageable target = collision.GetComponent<IDamageable>();
             if(target != null)
