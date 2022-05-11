9473b29236c736e5f4c167db4bb1b35bcb762b75
CRUSH04 <msng0408@naver.com>
2022-05-11 20:29:59 +0900

    Hpbar 리펙토링

--- Assets/02.Scripts/UI/EnemyHPBar.cs
+++ Assets/02.Scripts/UI/EnemyHPBar.cs
@@ -5,16 +5,11 @@ using UnityEngine.UI;
 
 public class EnemyHPBar : HpBar
 {
-    
-    private void Start()
-    {
-        // SetInitPosition();
-    }
 
     public void SetInitPosition()
     {
         Collider2D col = GetComponentInParent<Collider2D>();
-        float y = (col.bounds.size.y / 2) + 0.6f;
+        float y = -(col.bounds.size.y / 2) + 0.2f;
         transform.localPosition = new Vector3(0, y, 0);
     }
 }
