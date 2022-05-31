3bf97ace8efde671db6ecfee6d90d8bd252efbaf
July0313 <choiheejin5959@gmail.com>
2022-05-31 15:38:59 +0900

    Update WaterPriestess.cs

--- Assets/02.Scripts/Enemy/WaterPriestess.cs
+++ Assets/02.Scripts/Enemy/WaterPriestess.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class WaterPriestess : BossBase
 {
-    public int hitCount;
+    // public int hitCount;
+
+    public IEnumerator delayCoroutine;
 
     public override void Init()
     {
         base.Init();
         myFsm = Global.EnemyFsm.Idle;
+        myType = Global.EnemyType.Walking;
         speed = 6f;
-        hitCount = 0;
 
         sightDistance = 10f;    // 시야 범위
         attackDistance = 6f;    // 공격 범위
         rightDirection = Vector3.one;
-        leftDirection = new Vector3(rightDirection.x, rightDirection.y, rightDirection.z);
+        leftDirection = new Vector3(-rightDirection.x, rightDirection.y, rightDirection.z);
     }
 
     private void Start()
@@ -24,30 +26,48 @@ public class WaterPriestess : BossBase
         Init();
     }
 
-    private void Update()
+    private void CheckTransition()
     {
-        if (!isDie)
+        if (getHit)
         {
-            FsmUpdate();
+            StartState(Global.EnemyFsm.GetHit);
+            return;
         }
-    }
 
-    public void FsmUpdate()
-    {
         switch (myFsm)
         {
             case Global.EnemyFsm.Idle:
+                if(delayCoroutine == null)
+                {
+                    delayTime = Random.Range(1.4f, 2.0f);
+                    delayCoroutine = Delay(delayTime, Global.EnemyFsm.Chase);
+                    StartCoroutine(delayCoroutine);
+                }
                 break;
             case Global.EnemyFsm.Chase:
-                if (DistanceDecision(attackDistance))   // 공격 범위 내로 진입하면 Attack으로 상태 변경
+                if (DistanceDecision(attackDistance))
                 {
-                    ChangeState(Global.EnemyFsm.Attack);
+                    StartState(Global.EnemyFsm.Attack);
                 }
-                Chase();
                 break;
             case Global.EnemyFsm.Attack:
+                if (!isAttacking)
+                {
+                    StartState(Global.EnemyFsm.Chase);
+                }
+                break;
+            case Global.EnemyFsm.GetHit:
+                if (!getHit)
+                {
+                    StartState(Global.EnemyFsm.Idle);
+                }
                 break;
         }
+    }
+
+    private void StartState(Global.EnemyFsm state)
+    {
+        ChangeState(state);
     }
 
     public override void Attack()
@@ -65,5 +85,12 @@ public class WaterPriestess : BossBase
     public override void DeadAnimScript()   // 이렇게 할 거면 override가 필요한가??
     {
         base.DeadAnimScript();  // Destroy(this.gameObject)
+    }
+
+    public IEnumerator Delay(float delay, Global.EnemyFsm enemyFsm)
+    {
+        yield return new WaitForSeconds(delay);
+        StartState(enemyFsm);
+        delayCoroutine = null;
     }
 }
