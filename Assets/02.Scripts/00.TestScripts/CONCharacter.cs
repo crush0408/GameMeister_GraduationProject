86ddacf56c4d53b6839e24a59a04b8f0c2392c25
CRUSH04 <msng0408@naver.com>
2022-06-16 20:28:37 +0900

    적구조

---
+++ Assets/02.Scripts/00.TestScripts/CONCharacter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class CONCharacter : MonoBehaviour
+{
+    void Start()
+    {
+        SetTime();
+        Debug.Log(tt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/02.Scripts/00.TestScripts/CONCharacterFSM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class CONCharacter : MonoBehaviour
+{
+    float tt = 0f;
+
+    public void SetTime()
+    {
+        tt = 10f;
+    }
+}
