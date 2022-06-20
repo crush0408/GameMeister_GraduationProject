76c05d4bf2e977b853acc8ec62ed00c22e641c88
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-06-20 21:17:56 +0900

    벽 사라지는거

---
+++ Assets/02.Scripts/Map/BreakWall.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWall : MonoBehaviour
+{
+    public float damage = 0f;
+    public bool push = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BoxCollider2D boxCollider = collision.GetComponent<BoxCollider2D>();
+
+        if (collision.gameObject.CompareTag("Player") && boxCollider == collision)
+        {
+            IDamageable target = collision.GetComponent<IDamageable>();
+            if (target != null)
+            {
+                if(gameObject.activeSelf)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
