b69a57b7b64c0142040aa4036d17459b782e9a1c
July0313 <choiheejin5959@gmail.com>
2022-06-21 10:04:10 +0900

    Update Thronbush.cs

--- Assets/02.Scripts/Map/Thronbush.cs
+++ Assets/02.Scripts/Map/Thronbush.cs
@@ -5,18 +5,54 @@ using UnityEngine;
 public class Thronbush : MonoBehaviour
 {
     public float damage = 0f;
+    public float delay = 2f;
     public bool push = false;
+
+    private bool isTouched = false;
+    private IEnumerator delayCoroutine = null;
+
+    private void Update()
+    {
+        Debug.Log(isTouched);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         BoxCollider2D boxCollider = collision.GetComponent<BoxCollider2D>();
 
-        if (collision.gameObject.CompareTag("Player") && boxCollider == collision)
+        if (boxCollider.gameObject.CompareTag("Player"))
         {
+            isTouched = true;
+
             IDamageable target = collision.GetComponent<IDamageable>();
             if (target != null)
             {
-                target.OnDamage(damage, transform.position, push);
+                Debug.Log("진입");
+
+                delayCoroutine = Delay(delay, target);
+                StartCoroutine(delayCoroutine);
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        BoxCollider2D boxCollider = collision.GetComponent<BoxCollider2D>();
+
+        if (boxCollider.gameObject.CompareTag("Player"))
+        {
+            isTouched = false;
+
+            delayCoroutine = null;
+        }
+    }
+
+    public IEnumerator Delay(float delay, IDamageable target)
+    {
+        while (isTouched)
+        {
+            target.OnDamage(damage, transform.position, push);
+            yield return new WaitForSeconds(delay);
         }
     }
 }
