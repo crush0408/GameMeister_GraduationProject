89bddaa1714efe5a843249655e32334d3c9e034f
July0313 <choiheejin5959@gmail.com>
2022-06-22 20:17:49 +0900

    JumpBoard
    점핑 플랫폼

---
+++ Assets/02.Scripts/Map/Obstacles/JumpBoard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBoard : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    public int jumpForce = 30;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rigid = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if (rigid.velocity.y < 0)  // collision.gameObject.GetComponent<PlayerMove>().isGround
+        {
+            Debug.Log("점프 중이고 점프 발판에 닿았음");
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
