9af6aaa0c66e157326dfc5fa34bd7a59edfcaac6
July0313 <choiheejin5959@gmail.com>
2022-06-23 12:06:22 +0900

    Bomb

--- Assets/02.Scripts/Map/Obstacles/Bomb.cs
+++ Assets/02.Scripts/Map/Obstacles/Bomb.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private PlayerAttack playerAttack;
+
+    [Header("폭발 반경 및 데미지")]
+    public float bombRadius = 2f;
+    public float damage = 10f;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, bombRadius);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag("Enemy"))
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        playerAttack = collision.gameObject.GetComponentInParent<PlayerAttack>();
+
+        if (playerAttack.isAttacking)
         {
-            spriteRenderer.color = Color.green; // 시험용
+            spriteRenderer.color = Color.green; // 확인용(현재 애니메이션이 없어서ㅠㅠ)
+
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, bombRadius);
+            foreach (Collider2D col in colliders)
+            {
+                if (col.gameObject.CompareTag("Enemy"))
+                {
+                    Debug.Log("에너미 폭발");
+                    col.gameObject.GetComponent<EnemyHealth>().OnDamage(damage, transform.position, true);
+                }
+            }
         }
     }
 
