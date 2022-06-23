5e4c0b9647575ceb2b768e144ee243104477f3c6
July0313 <choiheejin5959@gmail.com>
2022-06-23 14:52:36 +0900

    Obstacle(Shooter)

--- Assets/02.Scripts/Map/Obstacles/Bullet.cs
+++ Assets/02.Scripts/Map/Obstacles/Bullet.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("구속 및 데미지")]
+    public float speed = 10f;
+    public float damage = 5f;
+
+    private Transform parentTransform;
+    // private Vector3 direction;
+
+    private void OnEnable()
     {
-        
+        parentTransform = GetComponentInParent<Transform>();
+
+        transform.position = parentTransform.position;
+        transform.rotation = parentTransform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.GetComponent<PlayerHealth>().OnDamage(damage, transform.position);
+
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Assets/02.Scripts/Map/Obstacles/Shooter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    [Header("총알 생성 간격")]
+    float time = 1f;
+
+    private void Start()
+    {
+        CreateBullet();
+    }
+
+    private void CreateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.SetParent(transform, false);
+
+        Invoke("CreateBullet", time);
+    }
+}
