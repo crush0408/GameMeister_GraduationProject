50477c77e28927d2efb77568547be136a1958457
July0313 <choiheejin5959@gmail.com>
2022-06-24 15:28:35 +0900

    Update ItemScript.cs

--- Assets/02.Scripts/Map/ItemScript.cs
+++ Assets/02.Scripts/Map/ItemScript.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     public GameObject tooltipObject;
+    private RectTransform _tooltipRectTrm;
+    private RectTransform _canvasRectTrm;
     public float dist = 2f;
 
     private Vector3 tooltipPos;
     private bool isRight = false;
-
+    private Camera mainCam;
     private void Start()
     {
+        mainCam = Camera.main;
         tooltipObject.SetActive(false);
 
         tooltipPos = Camera.main.ScreenToWorldPoint(tooltipObject.transform.position);
+        _tooltipRectTrm = tooltipObject.GetComponent<RectTransform>();
+        _canvasRectTrm = tooltipObject.transform.parent.GetComponent<RectTransform>();
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -24,24 +29,30 @@ public class ItemScript : MonoBehaviour
             isRight = gameObject.transform.position.x < col.transform.position.x ? true : false;
             Debug.Log("isRight : " + isRight);
 
-            tooltipObject.transform.position = isRight ?
-                Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x - dist, tooltipPos.y))
-                : Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x + dist, tooltipPos.y));
+            Vector3 leftPos = mainCam.WorldToScreenPoint(gameObject.transform.position + new Vector3(0, dist, 0));
+            //leftPos *= _canvasRectTrm.localScale.x;
+            //_tooltipRectTrm.anchoredPosition  = isRight ?
+            //    mainCam.WorldToScreenPoint(new Vector3(gameObject.transform.position.x - dist, tooltipPos.y))
+            //    : leftPos;
+            _tooltipRectTrm.position = leftPos;
 
-            Debug.Log(Camera.main.ScreenToWorldPoint(tooltipObject.transform.position));
+
+            Debug.Log(leftPos);
+
+            Debug.Log(mainCam.WorldToScreenPoint(new Vector3(gameObject.transform.position.x + dist, tooltipPos.y)));
             tooltipObject.SetActive(true);
         }
     }
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        if (!col.gameObject.CompareTag("Player")) return;
+        //if (!col.gameObject.CompareTag("Player")) return;
 
-        isRight = gameObject.transform.position.x < col.transform.position.x ? true : false;
+        //isRight = gameObject.transform.position.x < col.transform.position.x ? true : false;
 
-        tooltipObject.transform.position = isRight ?
-            Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x - dist, -2))
-            : Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x + dist, -2));
+        //tooltipObject.transform.position = isRight ?
+        //    Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x - dist, -2))
+        //    : Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x + dist, -2));
     }
 
     private void OnTriggerExit2D(Collider2D collision)
