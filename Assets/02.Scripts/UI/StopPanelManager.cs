efc6eaea0ec1cf0fc37a95ce7699da3f3e22f1ff
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-06-28 13:55:57 +0900

    버그 하나 고침

--- Assets/02.Scripts/UI/StopPanelManager.cs
+++ Assets/02.Scripts/UI/StopPanelManager.cs
@@ -11,7 +11,7 @@ public class StopPanelManager : MonoBehaviour
     public void ToGame()
     {
         StopPanel.SetActive(false);
-        Time.timeScale = 0;
+        Time.timeScale = 1;
     }
 
     public void ToMainMenu()
