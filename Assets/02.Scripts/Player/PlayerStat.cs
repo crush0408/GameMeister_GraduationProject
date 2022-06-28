73efc3113fce7eecb53e6d5747c3b1b9657888ce
July0313 <choiheejin5959@gmail.com>
2022-06-28 21:02:17 +0900

    -

--- Assets/02.Scripts/Player/PlayerStat.cs
+++ Assets/02.Scripts/Player/PlayerStat.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class PlayerStat : MonoBehaviour
 {
-    public enum PlayerType
-    {
-        NONE,
-        WATER_POWER,
-        LIGHTNING_SPEED,
-        ICE_BALANCE
-    }
-
-    [SerializeField] protected PlayerType _myType;
     [SerializeField] protected int _hp;
     [SerializeField] protected int _atk;
     [SerializeField] protected int _def;
     [SerializeField] protected int _pass;
     [SerializeField] protected float _atkSpeed;
     [SerializeField] protected int _moveSpeed;
+
     [SerializeField] protected int _fragment;   // 재화
 
-    public PlayerType MyType { get { return _myType; } set { _myType = value; } }
     public int HP { get { return _hp; } set { _hp = value; } }
     public int Attack { get { return _atk; } set { _atk = value; } }
     public int Defense { get { return _def; } set { _def = value; } }
@@ -32,14 +23,13 @@ public class PlayerStat : MonoBehaviour
 
     private void Start()
     {
-        _myType = PlayerType.NONE;
-
         _hp = 100;
         _atk = 70;
         _def = 30;
         _pass = 0;
         _atkSpeed = 0.7f;
         _moveSpeed = 5;
+
         _fragment = 0;
     }
 }
