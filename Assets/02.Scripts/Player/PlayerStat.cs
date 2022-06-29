e6bcc410071127a4377ac624425baa9d964cd10a
July0313 <choiheejin5959@gmail.com>
2022-06-29 18:19:05 +0900

    Complete playerStatPanel
    * 플레이 타임이 일시 정지 시간도 함께 계산하는 방식이라 전투 시간만 측정하는 타이머로 바꿔야 함

--- Assets/02.Scripts/Player/PlayerStat.cs
+++ Assets/02.Scripts/Player/PlayerStat.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerStat : MonoBehaviour
 {
+    public static PlayerStat instance = null;
+
     public enum PlayerType
     {
         NONE,
@@ -17,18 +19,32 @@ public class PlayerStat : MonoBehaviour
     [SerializeField] protected int _atk;
     [SerializeField] protected int _def;
     [SerializeField] protected int _pass;
-    [SerializeField] protected float _atkSpeed;
     [SerializeField] protected int _moveSpeed;
     [SerializeField] protected int _fragment;   // 재화
+    [SerializeField] protected float _atkSpeed;
+    [SerializeField] protected float _startTime;
 
     public PlayerType MyType { get { return _myType; } set { _myType = value; } }
     public int HP { get { return _hp; } set { _hp = value; } }
     public int Attack { get { return _atk; } set { _atk = value; } }
     public int Defense { get { return _def; } set { _def = value; } }
     public int Pass { get { return _pass; } set { _pass = value; } }
-    public float AttackSpeed { get { return _atkSpeed; } set { _atkSpeed = value; } }
     public int MoveSpeed { get { return _moveSpeed; } set { _moveSpeed = value; } }
     public int Frangment { get { return _fragment; } set { _fragment = value; } }
+    public float AttackSpeed { get { return _atkSpeed; } set { _atkSpeed = value; } }
+    public float StartTime { get { return _startTime;  } set { _startTime = value; } }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     private void Start()
     {
@@ -38,8 +54,10 @@ public class PlayerStat : MonoBehaviour
         _atk = 70;
         _def = 30;
         _pass = 0;
-        _atkSpeed = 0.7f;
         _moveSpeed = 5;
         _fragment = 0;
+        _atkSpeed = 0.7f;
+
+        _startTime = Time.time;
     }
 }
--- Assets/02.Scripts/UI/StatPanel.cs
+++ Assets/02.Scripts/UI/StatPanel.cs
@@ -6,23 +6,68 @@ using UnityEngine.UI;
 public class StatPanel : MonoBehaviour
 {
     public Image profileBackground;
-    public Image playerProfile;
 
-    public List<Text> statText;
+    [Header("스탯 텍스트 표시")]
+    public Text atkText;
+    public Text defText;
+    public Text passText;
+    public Text atkSpeedText;
+    public Text moveSpeedText;
 
-    private void Start()
+    [Space]
+    public Text timeText;
+    private int timeTemp;
+    private int hour = 0;
+    private int minute = 0;
+    private int second = 0;
+
+    private void OnEnable()
     {
-        foreach (Text text in statText)
+        atkText.text = "공격력 : " + PlayerStat.instance.Attack.ToString();
+        defText.text = "방어력 : " + PlayerStat.instance.Defense.ToString();
+        passText.text = "방어관통력 : " + PlayerStat.instance.Pass.ToString();
+        atkSpeedText.text = "공격 속도 : " + PlayerStat.instance.AttackSpeed.ToString();
+        moveSpeedText.text = "이동 속도 : " + PlayerStat.instance.MoveSpeed.ToString();
+
+        second = (int)(Time.time - PlayerStat.instance.StartTime);
+
+        if (second > 59 && minute > 59)
         {
-            text.text = Random.Range(0, 100).ToString();
+            minute = 0;
+            hour++;
+        }
+        else if (second > 59)
+        {
+            PlayerStat.instance.StartTime = Time.time;
+            second = 0;
+            minute++;
+        }
+
+        timeText.text = string.Format("{0:00} : {1:00} : {2:00}", hour, minute, second);
+
+        // 플레이어 프로필 백그라운드 색상 조정
+        switch (PlayerStat.instance.MyType)
+        {
+            case PlayerStat.PlayerType.NONE:
+                profileBackground.color = Color.black;
+                break;
+            case PlayerStat.PlayerType.ICE_BALANCE:
+                profileBackground.color = Color.blue;
+                break;
+            case PlayerStat.PlayerType.LIGHTNING_SPEED:
+                profileBackground.color = Color.yellow;
+                break;
+            case PlayerStat.PlayerType.WATER_POWER:
+                profileBackground.color = Color.cyan;
+                break;
         }
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            this.gameObject.SetActive(false);
+            profileBackground.color = Color.black;
         }
     }
 }
