81d21aedd20fd76669e48bf853d9878b84f91a2f
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-07-03 19:21:08 +0900

    불 플립 버그 수정

--- Assets/02.Scripts/Enemy/FireWarmBall.cs
+++ Assets/02.Scripts/Enemy/FireWarmBall.cs
@@ -10,18 +10,44 @@ public class FireWarmBall : MonoBehaviour
 
     private Transform fireWarmPosition;
 
+    public Vector3 rightDirection;
+    public Vector3 leftDirection;
+
+    private GameObject player;
+    private bool isRight = false;
+
     private void OnEnable()
     {
         fireWarmPosition = GetComponentInParent<Transform>();
 
         transform.position = fireWarmPosition.position;
         transform.rotation = fireWarmPosition.rotation;
+
+        rightDirection = Vector3.one;
+        leftDirection = new Vector3(-rightDirection.x, rightDirection.y, rightDirection.z);
+        player = GameObject.Find("Player");
+
+        Vector2 dir = player.transform.position - this.transform.position;
+        if (dir.x > 0)
+        {
+            transform.localScale = rightDirection;
+            isRight = true;
+        }
+        else
+        {
+            transform.localScale = leftDirection;
+            isRight = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if(isRight)
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        else
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
