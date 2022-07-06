906cadd80247293e2f1e447015e5c118788a0ca0
김예리나 <ggm.s200001@ggh.goe.go.kr>
2022-07-07 08:09:31 +0900

    6스테이지까지 버그 수정 완료

--- Assets/02.Scripts/Map/Obstacles/JumpBoard.cs
+++ Assets/02.Scripts/Map/Obstacles/JumpBoard.cs
@@ -10,12 +10,17 @@ public class JumpBoard : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        rigid = collision.gameObject.GetComponent<Rigidbody2D>();
 
-        if (rigid.velocity.y < 0)  // collision.gameObject.GetComponent<PlayerMove>().isGround
+
+        if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("점프 중이고 점프 발판에 닿았음");
-            rigid.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            rigid = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rigid.velocity.y < 0)  // collision.gameObject.GetComponent<PlayerMove>().isGround
+            {
+                Debug.Log("점프 중이고 점프 발판에 닿았음");
+                rigid.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
+
     }
 }
